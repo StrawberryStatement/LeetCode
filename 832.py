dcbe22fdb8261f7276cc94ae37565a23eac4e8d2
Trick150 <trick1500@163.com>
2018-06-03 22:11:12 +0800

    832
    用for迭代方式遍历列表时不能修改列表，需要用下表操作进行修改

---
+++ 832.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  3 15:22:46 2018
+832
+@author: trick150
+"""
+class Solution(object):
+    def flipAndInvertImage(self, A):
+        """
+        :type A: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        for i in range(len(A)):
+            A[i]=A[i][::-1]        
+        for j in range(len(A)):
+            for k in range(len(A[j])):
+                A[j][k]=1-A[j][k]
+        print(A)
+        return A
+A=[[1,1,0],[1,0,1],[0,0,0]]
+a=Solution()
+a.flipAndInvertImage(A)
