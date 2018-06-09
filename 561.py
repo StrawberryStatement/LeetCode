77fefcef7fbb9dc6af002502bad9c93d10b6f7f0
Trick150 <trick1500@163.com>
2018-06-09 23:31:40 +0800

    561

---
+++ 561.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  9 23:05:04 2018
+561
+@author: trick150
+"""
+from functools import reduce
+class Solution:
+    def arrayPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        #sort 函数返回NoneType
+        return sum(nums[::2])
+a=Solution()
+print(a.arrayPairSum([1,4,3,2]))
