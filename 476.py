286c57ee5f4d53abb269d4d828a1393b333a6cf7
Trick150 <trick1500@163.com>
2018-06-13 12:32:33 +0800

    476,、821comment

---
+++ 476.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 13 11:34:56 2018
+476
+@author: trick150
+"""
+class Solution:
+    def findComplement(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        l=len(bin(num))-2 
+        a=int("1"*l,2)
+        return num^a
+a=Solution()
+print(a.findComplement(5))
