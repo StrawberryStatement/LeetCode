313e18b1809b6f925fea4b87309860c493ce74b0
strawberrystatement <trick1500@163.com>
2018-10-18 10:00:55 +0800

    122，不用动态规划
    股票交易只要后一天比前一天的钱多就卖恰好是最大收益

---
+++ 122.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 18 09:46:19 2018
+
+122. Best Time to Buy and Sell Stock II
+
+@author: Trick150
+"""
+class Solution:
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        profit=0
+        for i in range(len(prices)-1):
+            if prices[i+1]>prices[i]:
+                profit+=prices[i+1]-prices[i]
+        return profit
